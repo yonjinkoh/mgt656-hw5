b443f2ef903f5b2fae8c535d844da2ab14de49e7
April <april@spylight.com>
2015-10-09 20:49:16 +0000

    Tested

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ app.get('/', function(request, response){
     response.send('Hello MGT-656!');
 });
 
-app.get('/netid', function(request, response) {
-    response.send('yk363');
+app.get('/nickname', function(request, response) {
+    response.send('yawning-yam');
 });
 
 app.listen(process.env.PORT || 4000,
